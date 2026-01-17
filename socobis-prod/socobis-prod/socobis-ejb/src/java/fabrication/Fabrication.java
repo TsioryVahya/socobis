3445326b8d823c47175dc7a6db1cb90bf78eb405
Odilah <odilahrandrianjanahary@hotmail.com>
2026-01-17 14:02:37 +0300

    sitrana pr iaho

--- socobis-prod/socobis-prod/socobis-ejb/src/java/fabrication/Fabrication.java
+++ socobis-prod/socobis-prod/socobis-ejb/src/java/fabrication/Fabrication.java
@@ -1014,7 +1014,7 @@ public class Fabrication extends Of {
     }
     @Override
     public ClassMAPTable createObject(MapUtilisateur u, Connection c)throws Exception{
-        if((u.getIdrole().compareTo(ConstanteSocobis.CHEFFABR_RANG) != 0)){
+        if(u.getIdrole().compareTo(ConstanteSocobis.CHEFFABR_RANG) != 0 && u.getIdrole().compareTo("cheffab") != 0){
             throw new Exception("Vous n’avez pas le droit de creer une fabrication");
         }
         return super.createObject(u,c);
